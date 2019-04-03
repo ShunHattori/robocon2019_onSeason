22df12f382db9b8e21e9d8974b3b5124064617f2
ShunHattori <kirarinice@gmail.com>
2019-04-03 18:55:54 +0900

    add:Location controller

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include "mbed.h"
 #include "Encoder.h"
 #include "MWodometry.h"
+#include "DriveTrain.h"
 
 Encoder encoder_XAxis_1(PD_15, PF_12);
 MWodometry odometry_XAxis_1(encoder_XAxis_1, 48, 5);
+DriveTrain<int> Robot(45, 83, 125);
 
 Serial PC(USBTX, USBRX);
 DigitalOut LED(LED1);
@@ -13,7 +15,9 @@ InterruptIn enctest(PF_12);
 
 void button_pressed()
 {
-    //PC.printf("Pulse:%ld\r\n",encoder_XAxis_1.getPulse());
+    PC.printf("%d\n\r", Robot.getXData());
+    PC.printf("%d\n\r", Robot.getYData());
+    PC.printf("%d\n\r", Robot.getYawData());
 }
 
 void test()
@@ -31,10 +35,16 @@ int main()
     //PC.printf("got encoder pulse:%ld\r\n", encoder_XAxis_1.getPulse());
     LED = 1;
 
-    encoder_XAxis_1.EnableDebugOutput(PB_7);
+    //encoder_XAxis_1.EnableDebugOutput(PB_7);
+    //Robot.setCurrentPoint(100, 642, 45);
+    PC.printf("%d\n\r", Robot.getXData());
+    PC.printf("%d\n\r", Robot.getYData());
+    PC.printf("%d\n\r", Robot.getYawData());
+    wait(10);
+    Robot.setCurrentPoint(90, 42, 531);
     for (;;)
     {
-        PC.printf("encoder pulse:%ld\todometry value:%ld\n\r", encoder_XAxis_1.getPulse(), odometry_XAxis_1.getDistance());
+        //PC.printf("encoder pulse:%ld\todometry value:%ld\n\r", encoder_XAxis_1.getPulse(), odometry_XAxis_1.getDistance());
         wait(0.2);
     }
 }
